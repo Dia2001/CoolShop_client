facfc195e4d0bc73d649d242e6a6ef0e2c68cb11
ongcaoboi <tuananhafiy@gmail.com>
2022-10-27 08:32:03 +0700

    feature - load dataa cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,55 +1,53 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import FooterTransition from './components/FooterTransition'
 import img from "../../assets/product-o1.png";
 import ItemsCart from './components/ItemsCart';
 import HeaderCart from './components/HeaderCart';
+import { AppContext } from '../../Providers/AppContext'
+import { ProductContext } from '../../Providers/ProductContext'
+import CartService from '../../services/CartService';
+import config from '../../config';
 
 const Cart = () => {
-  const listProduct = [
-    //test code
-    {
-      name: "Quần jeans nam Ống suông co giãn thoáng mát",
-      description: "Xanh, size: XS",
-      amount: "2",
-      price: "400000Đ",
-      img: img,
-    },
-    {
-      name: "Quần jeans nam Ống suông co giãn thoáng mát",
-      description: "Xanh, size: XS",
-      amount: "2",
-      price: "400000Đ",
-      img: img,
-    },
-    {
-      name: "Quần jeans nam Ống suông co giãn thoáng mát",
-      description: "Xanh, size: XS",
-      amount: "2",
-      price: "400000Đ",
-      img: img,
-    },
-    {
-      name: "Quần jeans nam Ống suông co giãn thoáng mát Quần jeans nam Ống suông co giãn thoáng mát",
-      description: "Xanh, size: XS",
-      amount: "2",
-      price: "400000Đ",
-      img: img,
-    },
-    {
-      name: "Quần jeans nam Ống suông co giãn thoáng mát",
-      description: "Xanh, size: XS",
-      amount: "2",
-      price: "400000Đ",
-      img: img,
-    },
-  ];
+  const { carts, setCarts } = useContext(AppContext)
+  const { isChange, findColorById, findSizeById } = useContext(ProductContext)
+  const [listProduct, setListProduct] = useState([])
+
+  useEffect(() => {
+    fetchApiCart()
+  }, [isChange])
+
+  const fetchApiCart = async () => {
+    const result = await CartService.getAll()
+    console.log(result)
+
+    if (result.success) {
+      let productCarts = result.data.map((item) => {
+        let size = findSizeById(item.sizeId)
+        let color = findColorById(item.colorId)
+
+        let description = (color ? color.name : '') + ', ' + size ? size.name : ''
+
+        return {
+          name: item.productName,
+          description: description,
+          amount: item.quantity,
+          price: item.price,
+          img: config.urlImageProduct + item.productImage,
+        }
+      })
+      setListProduct(productCarts)
+      setCarts(productCarts)
+      console.log(productCarts)
+    }
+  }
   return (
     <div className="m-1 flex flex-col gap-2">
-      <HeaderCart length={listProduct.length}/>
-      {listProduct.map((item,index)=>(
-        <ItemsCart key={index} item={item}/>
+      <HeaderCart length={listProduct.length} />
+      {listProduct.map((item, index) => (
+        <ItemsCart key={index} item={item} />
       ))}
-      <FooterTransition/>
+      <FooterTransition />
     </div>
   )
 }
